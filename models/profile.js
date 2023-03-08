fc1691b1fb1a65fd124657478de3452838aedda1
imcoco <imcoco@gmail.com>
2023-03-08 17:15:07 -0500

    update profile model

--- models/profile.js
+++ models/profile.js
@@ -4,7 +4,9 @@ const Schema = mongoose.Schema
 
 const profileSchema = new Schema({
   name: String,
-  photo: String
+  photo: String,
+  dishes: { type: Schema.Types.ObjectId, ref: 'Menu' },
+  events: { type: Schema.Types.ObjectId, ref: 'Event' }
 },{
   timestamps: true,
 })
