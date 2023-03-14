fc0a6ff6358ffa43b7f85c2b5cd7381429bc71e4
imcoco <imcoco@gmail.com>
2023-03-14 11:35:36 -0400

    edit profile model: dishes & events into array

--- models/profile.js
+++ models/profile.js
@@ -5,8 +5,8 @@ const Schema = mongoose.Schema
 const profileSchema = new Schema({
   name: String,
   photo: String,
-  dishes: { type: Schema.Types.ObjectId, ref: 'Dish' },
-  events: { type: Schema.Types.ObjectId, ref: 'Event' }
+  dishes: [{ type: Schema.Types.ObjectId, ref: 'Dish' }],
+  events: [{ type: Schema.Types.ObjectId, ref: 'Event' }]
 },{
   timestamps: true,
 })
