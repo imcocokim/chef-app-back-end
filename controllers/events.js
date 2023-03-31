801881a02e397664149f110d8caced743240f4ea
imcoco <imcoco@gmail.com>
2023-03-31 00:07:48 -0400

    add filter option to events create controller function

--- controllers/events.js
+++ controllers/events.js
@@ -1,9 +1,20 @@
 import { Event } from '../models/event.js'
+import { Dish } from '../models/dish.js'
+import { Filter } from '../models/filter.js'
 
 
 const create = async (req, res) => {
   try {
+    req.body.author = req.user.profile
     const event = await Event.create(req.body)
+
+    const filter = await Filter.findById(req.body.restrictions)
+
+    if (!filter.events.includes(event._id)) {
+      filter.events.push(event._id)
+      await filter.save()
+    }
+
     res.status(201).json(event)
   } catch (err) {
     res.status(500).json(err)
